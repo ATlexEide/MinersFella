e1e929be7014c37c51f884f3cfd57e8c52208eb2
Alexander Eide <atlexeide@gmail.com>
2026-05-17 20:09:33 +0200

    Add block break logic

---
+++ src/main/java/com.velvetthebnuuy.minersfella/Events.java
@@ -0,0 +1,90 @@
+package com.velvetthebnuuy.minersfella;
+
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+
+public class Events implements Listener {
+
+	int sum = 0;
+
+	@EventHandler
+	public void onBlockBreak(BlockBreakEvent event) {
+		sum = 0;
+		String itemInHand = Objects
+			.requireNonNull(event.getPlayer().getEquipment())
+			.getItemInMainHand()
+			.getType()
+			.toString();
+		List<String> ALLOWED_TOOLS = Arrays.asList(
+			"NETHERITE_PICKAXE",
+			"STONE_PICKAXE",
+			"IRON_PICKAXE",
+			"GOLDEN_PICKAXE",
+			"WOODEN_PICKAXE",
+			"DIAMOND_PICKAXE"
+		);
+		if (!ALLOWED_TOOLS.contains(itemInHand)) {
+			return;
+		}
+
+		Block block = event.getBlock();
+
+		removeBlock(block);
+	}
+
+	public void removeBlock(Block block) {
+		String type = block.getType().name();
+
+		List<String> visited = new ArrayList<>();
+		Queue<Block> queue = new ArrayDeque<Block>();
+		queue.offer(block);
+
+		int blockCount = 0;
+		int blockLimit = 10000;
+		while (!queue.isEmpty()) {
+			try {
+				Block curr = queue.peek();
+				Location loc = curr.getLocation();
+				String currLocation =
+					Double.toString(loc.getX()) + Double.toString(loc.getY()) + Double.toString(loc.getZ());
+				visited.add(currLocation);
+
+				Block[] neighbours = {
+					curr.getRelative(BlockFace.UP),
+					curr.getRelative(BlockFace.DOWN),
+					curr.getRelative(BlockFace.NORTH),
+					curr.getRelative(BlockFace.SOUTH),
+					curr.getRelative(BlockFace.WEST),
+					curr.getRelative(BlockFace.EAST)
+				};
+
+				if (blockCount < blockLimit) {
+					for (Block neighbouringBlock : neighbours) {
+						loc = neighbouringBlock.getLocation();
+						currLocation =
+							Double.toString(loc.getX()) + Double.toString(loc.getY()) + Double.toString(loc.getZ());
+
+						boolean isSameType =
+							neighbouringBlock.getType().toString().equals(type) && !visited.contains(currLocation);
+						if (isSameType) {
+							visited.add(currLocation);
+							queue.offer(neighbouringBlock);
+							blockCount++;
+						}
+					}
+				}
+				curr.breakNaturally();
+				queue.remove();
+			} catch (Exception e) {
+				Bukkit.getLogger().warning(e.getMessage());
+			}
+		}
+	}
+}
