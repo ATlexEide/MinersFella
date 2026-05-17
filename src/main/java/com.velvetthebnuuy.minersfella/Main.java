546eff72da77efae96c9b9ca621eac8c77d980d4
Alexander Eide <atlexeide@gmail.com>
2026-05-17 20:08:36 +0200

    Register events

--- src/main/java/com.velvetthebnuuy.minersfella/Main.java
+++ src/main/java/com.velvetthebnuuy.minersfella/Main.java
@@ -1,11 +1,14 @@
 package com.velvetthebnuuy.minersfella;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
         @Override
         public void onEnable() {
             getLogger().info("onEnable is called!");
+            Bukkit.getPluginManager().registerEvents(new Events(), this);
+            getLogger().info("YAYYYYY!");
         }
         @Override
         public void onDisable() {
