d9af49501cc382ce2630df629705e09b5b880fd8
Alexander Eide <atlexeide@gmail.com>
2026-05-19 15:27:23 +0200

    Add config

--- src/main/java/com.velvetthebnuuy.minersfella/Main.java
+++ src/main/java/com.velvetthebnuuy.minersfella/Main.java
@@ -4,15 +4,16 @@ import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
-        @Override
-        public void onEnable() {
-            getLogger().info("onEnable is called!");
-            Bukkit.getPluginManager().registerEvents(new Events(), this);
-            getLogger().info("YAYYYYY!");
-        }
-        @Override
-        public void onDisable() {
-            getLogger().info("onDisable is called!");
-        }
-    }
 
+	@Override
+	public void onEnable() {
+		saveDefaultConfig();
+		Bukkit.getPluginManager().registerEvents(new Events(this), this);
+		getLogger().info("YAYYYYY!");
+	}
+
+	@Override
+	public void onDisable() {
+		getLogger().info("onDisable is called!");
+	}
+}
